03f51ecdadf04f152b167fe00df7f1bfcab33018
Dor <arnavim88@gmail.com>
2019-09-04 01:44:53 +0300

    current build added

---
+++ build/precache-manifest.f3a66e4417ff3b84f720036e995095a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7888ff7107c160ab38025f4adb06e2f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f2aece21cf8311a07b1",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e919742a97c48758aece",
+    "url": "/static/js/2.a8d70a82.chunk.js"
+  },
+  {
+    "revision": "5f2aece21cf8311a07b1",
+    "url": "/static/js/main.41a018b0.chunk.js"
+  },
+  {
+    "revision": "bf12498eb9f6bffff8eb",
+    "url": "/static/js/runtime~main.7462fa69.js"
+  }
+]);
